ecc524159c62c73b829e05d3905c8ef4b2d277f3
sonksen <sonksen_michael@lilly.com>
2019-01-23 09:36:21 -0500

    updates

--- server.R
+++ server.R
@@ -35,19 +35,97 @@
 ######################################################################
 ## 
 ### Libraries:
-
-
+library(ggplot2)
+library(htmlwidgets)
 ## 
 ### Global Functions: 
+source("post.R")
 
 
 ## 
 ### Code: 
 shinyServer(function(input,output,clientData, session){
 
+    sim_results <- list()
+    sims <- NULL
+    
+    output$hot_scenarios <- renderRHandsontable({
+        df <- data.frame("Scenario Name"=c("Null", "Null SD=2", "Expected",
+                                  "Expected SD=2"),
+                         "ControlMean"=c(0.0,0.0,1.0,1.0),
+                         "ControlSD"=c(1.0,2.0,1.0,2.0),
+                         "TRTMean"=c(2.0,3.0,2.0,3.0),
+                         "TRTSD"=c(1.0,1.0,2.0,2.0))
+        rhandsontable(df, useTypes=FALSE,height=300*2,
+                    search=TRUE) %>%
+        hot_table(highlightCol=TRUE,highlightRow=TRUE) %>%
+            hot_context_menu(allowRowEdit=TRUE, allowColEdit=TRUE,
+                             allowComments=TRUE,
+                             customOpts = list(
+                             csv = list(name = "Download to CSV",
+                                      callback = htmlwidgets::JS(
+                                        "function (key, options) {
+                                        var csv = csvString(this, sep=',', dec='.');
+                                        
+                                        var link = document.createElement('a');
+                                        link.setAttribute('href', 'data:text/plain;charset=utf-8,' +
+                                        encodeURIComponent(csv));
+                                        link.setAttribute('download', 'data.csv');
+                                        
+                                        document.body.appendChild(link);
+                                        link.click();
+                                        document.body.removeChild(link);
+  }")))) %>%
+        hot_cols(columnSorting=TRUE,manualColumnMove=TRUE,
+                 manualColumnResize=TRUE)
+
+    })
 
 
+    output$prior_mix_plot <- renderPlot({
+        mn1 <- input$prior_mix_mean1
+        sd1 <- input$prior_mix_sd1
+        mn2 <- input$prior_mix_mean2
+        sd2 <- input$prior_mix_sd2
+        mix <- input$prior_mix_weight1
+        dnorm_c <- function(x) mix*dnorm(x,mn1,sd1)+(1-mix)*dnorm(x,mn2,sd2)
+        xvalues <- data.frame(x=c(min(mn1-3*sd1,mn2-3*sd2),
+                                  max(mn1+3*sd1,mn2+3*sd2)))
+        ggplot(xvalues,aes(x=xvalues))+
+            stat_function(fun=dnorm_c, geom="area",fill="turquoise",alpha=.3)+
+            xlim(c(min(mn1-4*sd1,mn2-4*sd2),max(mn2+4*sd2,mn1+4*sd1)))+
+            ##geom_vline(xintercept=input$eoi1,colour="red")+
+            labs(x="Variable", y="Density")
+    })
 
+    output$prior_power_plot <- renderPlot({
+        mn <- input$prior_power_mean
+        sd <- input$prior_power_sd
+        a0 <- input$prior_power_a0
+        dnorm_c <- function(x) dnorm(x,mn,sd)^(a0)
+        xvalues <- data.frame(x=c(mn-10*sd,mn-10*sd,
+                                  mn+10*sd,mn+10*sd))
+        ggplot(xvalues,aes(x=xvalues))+
+            stat_function(fun=dnorm_c, geom="area",fill="orange",alpha=.3)+
+            xlim(c(min(mn-11*sd,mn-11*sd),max(mn+11*sd,mn+11*sd)))+
+            ##geom_vline(xintercept=input$eoi1,colour="red")+
+            labs(x="Variable", y="Density")
+    })
+
+    output$design_csf <- renderUI({
+        withMathJax(paste0("P(TRT-Control > ",input$design_csf_eoi,")>",input$design_csf_prth))
+    })
+
+    ##Simulations
+    observeEvent(input$sim_go,
+    {
+        set.seed(input$sim_seed)
+        n_sims <- input$sim_n
+
+        ##generate sims
+        
+        
+    })
 })
 
 
--- ui.R
+++ ui.R
@@ -37,18 +37,107 @@
 ### Libraries:
 library(shiny)
 library(shinydashboard)
+library(rhandsontable)
 
 ## 
 ### Global Functions: 
 
 
+rm(list=ls())
+options(shiny.sanitize.errors=FALSE) ##informative error messages
+
 ## 
 ### Code: 
 
 dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody()
+    dashboardHeader(
+        title="Trial Simulator"
+    ),
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem("Scenarios", tabName="tab_scenarios"),
+            menuItem("Priors", tabName="tab_priors"),
+            menuItem("Design", tabName="tab_design"),
+            menuItem("Simulations", tabName="tab_simulations")
+        )
+    ),
+    dashboardBody(
+        tabItems(
+            tabItem(tabName="tab_scenarios",
+                fluidRow(
+                    box(title="Scenarios",rHandsontableOutput("hot_scenarios"))
+                )
+            ),
+            tabItem(tabName="tab_priors",
+                fluidRow(
+                    box(title="Two Component Mixture Prior",
+                        fluidRow(
+                            column(6,numericInput("prior_mix_weight1",
+                                                  "Mixture Prior Weight",
+                                                  value=0.5,min=0.0,max=1.0,
+                                                  step=0.1))
+                        ),
+                        fluidRow(
+                        column(6,numericInput("prior_mix_mean1",
+                                              "Mixture Prior Mean of Skeptical",
+                                              value=0.0,step=0.1)),
+                        column(6,numericInput("prior_mix_mean2",
+                                              "Mixture Prior Mean of Informative",
+                                              value=1.5,step=0.1))
+                        ),
+                        fluidRow(
+                        column(6,numericInput("prior_mix_sd1",
+                                              "Mixture Prior SD of Skeptical",
+                                              value=1.0,min=0.0,step=0.1)),
+                        column(6,numericInput("prior_mix_sd2",
+                                              "Mixture Prior SD of Informative",
+                                              value=3.0,min=0.0,step=0.1))
+                        )
+                    ),
+                    box(plotOutput("prior_mix_plot"))
+                ),
+                fluidRow(
+                    box(tabName="Power Priors",
+                        numericInput("prior_power_a0","Power Prior Discounting Parameter (a0)",
+                                     value=0.5,min=0.0,max=1.0,step=0.1),
+                        numericInput("prior_power_mean","Power Prior Mean",
+                                     value=0.5,step=0.1),
+                        numericInput("prior_power_sd", ## add plot without discount
+                                     "Power Prior Standard Deviation",
+                                     value=0.5,min=0.0,step=0.1)
+                    ),
+                    box(plotOutput("prior_power_plot"))
+                )
+            ),
+            tabItem(tabName="tab_design", ## Add Case Examples
+                fluidRow(
+                    box(tabName="Design",
+                        numericInput("design_n_control","Sample Size for Control",
+                                     value=100,min=0,step=1),
+                        numericInput("design_n_trt","Sample Size for TRT",
+                                     value=100,min=0,step=1)
+                    ),
+                    box(tabName="Success Criterion",
+                        "P(TRT-Control>EOI)>PRTH)",
+                        numericInput("design_csf_eoi", "EOI",
+                                     value=0.0,step=0.1),
+                        numericInput("design_csf_prth", "PRTH",
+                                     value=0.8,min=0.0,max=1.0,step=0.01),
+                        uiOutput("design_csf")
+                    )
+                )
+            ),
+            tabItem(tabName="tab_simulations",
+                fluidRow(
+                    box(tabName="Simulation Settings",
+                        numericInput("sim_seed",label="R random number seed",value=4212,step=1),
+                        numericInput("sim_n",label="Number of Simulations",value=100,step=1),
+                        actionButton("sim_go",label="Run Simulations!")
+                    )
+                )
+            )
+        )
+    ),title=''
 )
 
 
